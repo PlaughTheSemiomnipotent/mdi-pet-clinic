fb487547bb068517ed5396d577421de497d1de7b
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-02-24 12:48:36 -0500

    Add owner injection and display list. Closes #16

--- pet-clinic-web/src/main/java/com/MDI747/mdipetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/MDI747/mdipetclinic/controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package com.MDI747.mdipetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.MDI747.mdipetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-	
-	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
+
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
-	
+
 }
