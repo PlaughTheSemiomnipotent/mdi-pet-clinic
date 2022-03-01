5246cd9fe6227ca6797d301c41c619b1f46009f8
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-03-01 08:05:15 -0500

    added load of pet types. Closes #32

--- pet-clinic-web/src/main/java/com/MDI747/mdipetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/MDI747/mdipetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.MDI747.mdipetclinic.model.Owner;
+import com.MDI747.mdipetclinic.model.PetType;
 import com.MDI747.mdipetclinic.model.Vet;
 import com.MDI747.mdipetclinic.services.OwnerService;
+import com.MDI747.mdipetclinic.services.PetTypeService;
 import com.MDI747.mdipetclinic.services.VetService;
 
 @Component
@@ -13,14 +15,30 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		PetType bird = new PetType();
+		dog.setName("Bird");
+
+		PetType savedDogPetType = petTypeService.save(dog);
+		PetType savedCatPetType = petTypeService.save(cat);
+		PetType savedBirdPetType = petTypeService.save(bird);
+
+		System.out.println("Loaded PetTypes...");
+
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
