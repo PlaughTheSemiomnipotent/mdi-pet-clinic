0e49b16c7ba48609b3429f97c106740b243c7773
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-03-01 09:33:36 -0500

    Add specialty service and impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package com.MDI747.mdipetclinic.services;
+
+import com.MDI747.mdipetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
--- pet-clinic-web/src/main/java/com/MDI747/mdipetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/MDI747/mdipetclinic/bootstrap/DataLoader.java
@@ -74,7 +74,7 @@ public class DataLoader implements CommandLineRunner {
 
 		ownerService.save(owner2);
 
-		System.out.println("Loaded Owners...");
+		System.out.println("Loaded Owners and Pets...");
 
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Sam");
