9474086b69da3bf0eb15921e89e5462c916b74ff
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-03-02 16:07:47 -0500

    make Owner, Pet, PetType into

--- pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/model/Pet.java
@@ -2,10 +2,28 @@ package com.MDI747.mdipetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+
+	@Column(name = "name")
 	private String name;
+
+	@Column(name = "birth_date")
 	private LocalDate birthDate;
+
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType petType;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
 
 	public String getName() {
