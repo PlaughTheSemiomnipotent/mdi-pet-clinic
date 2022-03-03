c48587656f9c6a5dcc8d12b03714f6202cb7fb93
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-03-03 12:50:36 -0500

    Create JPA service for vets. Closes #41

---
+++ pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/services/jpa/VetJpaService.java
@@ -0,0 +1,50 @@
+package com.MDI747.mdipetclinic.services.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.MDI747.mdipetclinic.model.Vet;
+import com.MDI747.mdipetclinic.repositories.VetRepository;
+import com.MDI747.mdipetclinic.services.VetService;
+
+@Service
+@Profile("springdatajpa")
+public class VetJpaService implements VetService {
+	private final VetRepository vetRepository;
+
+	public VetJpaService(VetRepository vetRepository) {
+		super();
+		this.vetRepository = vetRepository;
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+		Set<Vet> vets = new HashSet<>();
+		vetRepository.findAll().forEach(vets::add);
+		return vets;
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return vetRepository.save(object);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		vetRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);
+	}
+
+}
