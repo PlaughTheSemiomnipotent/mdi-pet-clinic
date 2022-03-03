1b3d675e88215e44eec0d831ceabd1b19d28efe4
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-03-03 13:34:30 -0500

    Create JPA service for vet specialties, Closes #44

---
+++ pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/services/jpa/SpecialtyJpaService.java
@@ -0,0 +1,49 @@
+package com.MDI747.mdipetclinic.services.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.MDI747.mdipetclinic.model.Specialty;
+import com.MDI747.mdipetclinic.repositories.SpecialtyRepository;
+import com.MDI747.mdipetclinic.services.SpecialtyService;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtyJpaService implements SpecialtyService {
+	private final SpecialtyRepository specialtyRepository;
+
+	public SpecialtyJpaService(SpecialtyRepository specialtyRepository) {
+		this.specialtyRepository = specialtyRepository;
+	}
+
+	@Override
+	public Set<Specialty> findAll() {
+		Set<Specialty> specialties = new HashSet<>();
+		specialtyRepository.findAll().forEach(specialties::add);
+		return specialties;
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return specialtyRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Specialty save(Specialty object) {
+		return specialtyRepository.save(object);
+	}
+
+	@Override
+	public void delete(Specialty object) {
+		specialtyRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialtyRepository.deleteById(id);
+	}
+
+}
