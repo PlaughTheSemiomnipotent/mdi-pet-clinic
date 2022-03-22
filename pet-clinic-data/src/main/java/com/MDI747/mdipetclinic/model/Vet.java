744d66a2344831d8779b185d511be118311edb24
Matthew Ireland <MatthewDeanIreland@gmail.com>
2022-03-22 13:41:13 -0400

    Add Vet view specialty code. closes #63

--- pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/MDI747/mdipetclinic/model/Vet.java
@@ -24,14 +24,14 @@ import lombok.Setter;
 @Table(name = "vets")
 public class Vet extends Person {
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+	private Set<Specialty> specialties = new HashSet<>();
+
 	@Builder
 	public Vet(Long id, String firstName, String lastName, Set<Specialty> specialties) {
 		super(id, firstName, lastName);
 		this.specialties = specialties;
 	}
-
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
-	private Set<Specialty> specialties = new HashSet<>();
 
 }
